52a669ead8475be86a08fdb7b712be2cbf94cde8
Thibault Wittemberg <twittemberg@genetec.com>
2020-12-26 18:44:22 -0500

    statemachine: add unit test for Transition

--- Tests/FeedbacksTests/StateMachine/TransitionTests.swift
+++ Tests/FeedbacksTests/StateMachine/TransitionTests.swift
@@ -96,17 +96,15 @@ final class TransitionTests: XCTestCase {
 }
 
 extension TransitionTests {
-    func testBuildBlock_build_array_of_transitions_with_variadicParameterOfTransitions() {
-        // Given: 2 transitions
+    func testEquality() {
+        // Given: 3 transitions
         let transitionA = Transition(from: MockState.self, on: MockEvent.self, then: MockState(value: 1))
         let transitionB = Transition(from: AnotherMockState.self, on: AnotherMockEvent.self, then: MockState(value: 2))
+        let transitionC = Transition(from: MockState.self, on: MockEvent.self, then: MockState(value: 2))
 
-        // When: making an array of transitions thanks to the ResultBuilder
-        let receivedTransitions = TransitionsBuilder.buildBlock(transitionA, transitionB)
-
-        // Then: the array of transitions is composed of the input transitions
-        let receivedTransitionIds = receivedTransitions.flatMap { $0.entries.map { $0.key } }
-        let expectedTransitionsIds = ([transitionA, transitionB] as [Transitions]).flatMap { $0.entries.map { $0.key } }
-        XCTAssertEqual(receivedTransitionIds, expectedTransitionsIds)
+        // When: comparing them for their equality
+        // Then: A == C and A != B
+        XCTAssertEqual(transitionA, transitionC)
+        XCTAssertNotEqual(transitionA, transitionB)
     }
 }
